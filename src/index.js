963357a0a7d94b1d6e8be46ad57554e737584844
Swen <swenmeeu@gmail.com>
2019-05-28 22:28:25 +0200

    Establish database connection before starting webservice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
-const port = process.env.PORT || 433;
+const port = process.env.PORT || config.app.port || 433;
 
-// Configure routes
-const routes = require('./routes');
-app.use('/api', routes);
+// Set up database connection
+const mongoDbConfig = { useNewUrlParser: true };
+const mongoDb = process.env.MONGODB_URI || config.database.connectionString;
+mongoose.connect(mongoDb, mongoDbConfig);
 
-// Default API response
-app.get('/', (req, res) => {
-  res.sendStatus(200); // OK
-});
+const databaseConnection = mongoose.connection;
+databaseConnection.on('error', console.error.bind(console, 'Database connection error:'));
+databaseConnection.once('open', () => {
+  console.log('Successfully connected to database!');
 
-app.listen(port, () => {
-  console.log(`Server is up and running on port number ${port}`);
+  // Configure routes
+  const routes = require('./routes');
+  app.use('/api', routes);
+
+  // Default API response
+  app.get('/', (req, res) => {
+    res.sendStatus(200); // OK
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is up and running on port number ${port}`);
+  });
 });
